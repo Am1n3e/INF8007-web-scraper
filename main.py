befa6ae104e99baa26efb9eef854cf47781a8876
Amine El hattami <amine.elhattami@gmail.com>
2020-02-21 11:26:09 -0500

    Updated the Crawler class to remove static methods; Added doc and comment

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.crawler import Crawler
 
 def parse_args():
     arg_parser = argparse.ArgumentParser(description="Web crawler application")
-    arg_parser.add_argument("website_link", help="link to website to crawl")
+    arg_parser.add_argument("website_url", help="Url of the website to crawl")
 
     return arg_parser.parse_args()
 
@@ -14,10 +14,12 @@ def main():
     args = parse_args()
 
     try:
-        dead_links = Crawler.crawl(args.website_link)
-        print(f'dead links are: {" ".join(dead_links)}')
-    except Exception as e:
-        print(f"Error occured while crawling {args.website_link}. {str(e)}")
+        crawler = Crawler(args.website_url)
+        crawler.crawl()
+        print(f'dead links: {" ".join(crawler.dead_links)}')
+    except Exception as exception:
+        # Using Broad excetion to catch all errors to give a proper error message
+        print(f"Error occured while crawling {args.website_link}. {str(exception)}")
 
 
 if __name__ == "__main__":
--- src/crawler.py
+++ src/crawler.py
@@ -4,33 +4,57 @@ from src.scrapper import Scraper
 
 
 class Crawler:
-    @staticmethod
-    def crawl(source_web_page_link):
-        website_links = {"visited": [], "dead": []}
+    def __init__(self, website_url):
+        """Init the crawler object.
 
-        website_links["visited"].append(source_web_page_link)
+        Args:
+            source_web_page_link: The web site link to crawl
+        """
+        # By using the '__' it will create a "private" var effect
+        # Since mangling variables names is required to access the value
+        self.__visited_links = []
+        self.__dead_links = []
+        self.__website_url = website_url
 
-        if Crawler._is_dead_link(source_web_page_link):
-            # If the page is not accessible there is no need to continue
-            raise ValueError(f"The source web page link ({source_web_page_link}) is not accessible")
+    @property
+    def dead_links(self):
+        """Get the dead links.
 
-        Crawler._crawl(source_web_page_link, "/", website_links)
+        Returns:
+            The dead links list
+        """
+        # Using a property to make sure that the dead links can't be modified (unless you mangle the name)
+        return self.__dead_links
 
-        return website_links["dead"]
+    def clear(self):
+        """Clears the visited and dead links lists"""
+        self.__visited_links = []
+        self.__dead_links = []
 
-    @staticmethod
-    def _crawl(source_web_page_link, route, website_links):
-        links = Scraper.get_web_page_links(source_web_page_link + route)
+    def crawl(self):
+        """Crawl the website url given to the init"""
+        self.clear()
+
+        if Crawler._is_dead_link(self.__website_url):
+            # If the web site is not accessible in the first place there is no need to continue
+            raise ValueError(f"The source web page link ({self.__website_url}) is not accessible")
+
+        self.__visited_links.append(self.__website_url)
+
+        self._crawl(self.__website_url, "/")  # the "/" is for the root route (the website page it self)
+
+    def _crawl(self, source_link, route):
+        """Crawl the page (source_link + route) given to the init"""
+        links = Scraper.get_web_page_links(source_link + route)
         for link in links:
-            full_link = source_web_page_link + link if link.startswith("/") else link
-            if full_link not in website_links["visited"]:
-
-                website_links["visited"].append(full_link)
+            full_link = source_link + link if link.startswith("/") else link
+            if full_link not in self.__visited_links:
+                self.__visited_links.append(full_link)
 
                 if Crawler._is_dead_link(full_link):
-                    website_links["dead"].append(link)
-                elif link.startswith("/") or link.startswith(source_web_page_link):  # Internal links
-                    Crawler._crawl(source_web_page_link, link, website_links)
+                    self.__dead_links.append(full_link)
+                elif link.startswith("/") or link.startswith(source_link):  # Internal links
+                    self._crawl(source_link, link)
 
     @staticmethod
     def _is_dead_link(link):
@@ -43,8 +67,4 @@ class Crawler:
             True if the link is dead else False
         """
         response = requests.get(link)
-        return response.status_code != 200
-
-
-if __name__ == "__main__":
-    print(Crawler.crawl("https://webscraper.io"))
+        return response.status_code != 200  # Alive pages return 200 as http response status code
