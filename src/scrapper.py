b397bafc83c1f742fa850ede6648e98f8d8e3f35
Amine El hattami <amine.elhattami@gmail.com>
2020-02-21 13:14:24 -0500

    Updated link regexp Added comment

--- src/scrapper.py
+++ src/scrapper.py
@@ -8,6 +8,7 @@ class Scraper:
         response = requests.get(web_page_link)
 
         if response.status_code != 200:
+            # This function expext that link is not dead, so this is just to make sure upstream code is going the check
             raise RuntimeError(f"Unable to scrape {web_page_link}. Got status code = {response.status_code}")
 
         web_page_content = response.content.decode()
@@ -17,6 +18,6 @@ class Scraper:
             raise RuntimeError(f"Failed to extract web page body for {web_page_link}")
 
         body = match.groups()[0]
-        links = re.findall(r"<a href=[\'\"]?(/[^\'\"#>]+|http[s]?[^\r\n\t\f\v\"\']+)[\'\"]?", body)
+        links = re.findall(r"<a [\S]* ?href=[\'\"]?(/[^\'\"#>]+|http[s]?[^\r\n\t\f\v\"\']+)[\'\"]?", body)
 
         return links
