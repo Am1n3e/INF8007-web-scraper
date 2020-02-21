8b17222fd29d0c66693e64a7d40d06c9d9a9bec2
Amine El hattami <amine.elhattami@gmail.com>
2020-02-21 11:32:47 -0500

    Added the print_dead_links function

--- main.py
+++ main.py
@@ -10,13 +10,21 @@ def parse_args():
     return arg_parser.parse_args()
 
 
+def print_dead_links(dead_links):
+    if dead_links:
+        print(f'dead links: {" ".join(dead_links)}')
+    else:
+        print("No dead links found")
+
+
 def main():
     args = parse_args()
 
     try:
         crawler = Crawler(args.website_url)
         crawler.crawl()
-        print(f'dead links: {" ".join(crawler.dead_links)}')
+
+        print_dead_links(crawler.dead_links)
     except Exception as exception:
         # Using Broad excetion to catch all errors to give a proper error message
         print(f"Error occured while crawling {args.website_link}. {str(exception)}")
